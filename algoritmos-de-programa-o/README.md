16d8e5c955d40d194f216118d74600d0cf559760
Felipe Alejandro Oliveira Sousa <alejamdrem255@gmail.com>
2026-09-16 23:22:59 -0300

    Delete algoritmos-de-programa-o directory

--- algoritmos-de-programa-o/README.md
+++
@@ -1,1 +0,0 @@
-# algoritmos-de-programa-o
--- algoritmos-de-programa-o/aula3/Exercicio10.java
+++
@@ -1,8 +0,0 @@
-public class Exercicio10 {
-public static void main(String[] args) {
-    double lado, area;
-    lado = 350.0;
-    area = lado * lado;
-    System.out.println("A área do quadrado é " + area);
-}
-}
